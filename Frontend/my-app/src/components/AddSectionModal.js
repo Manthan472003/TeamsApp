003d0ffa5d1e33a426b01796f917caab5785c5f3
harsh7733 <harshvardhanrakate@gmail.com>
2024-09-05 17:47:31 +0530

    second commit

--- Frontend/my-app/src/components/AddSectionModal.js
+++ Frontend/my-app/src/components/AddSectionModal.js
@@ -76,6 +76,8 @@ const AddSectionModal = ({ isOpen, onClose, onSectionAdded }) => {
             onClose={onClose}
             size="md"
         >
+
+        <h1>asdfghjkl</h1>
             <ModalOverlay />
             <ModalContent>
                 <ModalHeader>Add Section</ModalHeader>
