6c0873bad39f00764a3bd802aeb90d524f9d0912
Manthan472003 <manthandeodhe@gmail.com>
2024-09-20 11:20:56 +0530

    fixed media

--- Backend/API/Routes/mediaRoutes.js
+++ Backend/API/Routes/mediaRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const mediaController = require('../Controllers/mediaController')
-// Route for creating a new image (Signup)
-router.post('/', mediaController.createMedia);
+const mediaController = require('../Controllers/mediaController');
 
-// Route for getting all images
+// Route for creating a new media file
+router.post('/', mediaController.upload.single('mediaFile'), mediaController.createMedia);
+
+// Route for getting all media
 router.get('/', mediaController.getAllMedias);
 
-// Route for getting a image by ID
+// Route for getting a media by ID
 router.get('/:id', mediaController.getMediaById);
 
-// Route for deleting a image by ID
+// Route for deleting a media by ID
 router.delete('/:id', mediaController.deleteMediaById);
 
 module.exports = router;
